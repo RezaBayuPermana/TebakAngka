3aaa467e38cc5e5e5a3366f6e485334e3de03567
Rheza Permana <Rheza Permana@RhezaPermana-PC>
2016-11-22 20:09:35 +0700

    12141378 Public digunakan untuk menyatakan bahwa kelas/method/attribute tersebut dapat diakses oleh kelas lain dimanapun. Public digunakan apabila kita menginginkan semua variable dalam Class tersebut dapat di akses dari Class manapun. ArrayList  adalah  kelas  yang  memungkinkan  pembuatan  list  objek  array  yang  ukurannya dapat berubah secara dinamis atau bisa dibilang ukuran ArrayList dapat berubah sesuai dengan jumlah data yang dimasukkan.  Hampir  mirip  seperti  fungsi  array,  arraylist  digunakan  untuk  menyimpan  data objek. Namun perbedaan dengan array biasa terletak pada tipe data dari objek yang akan disimpan ke dalam  arraylist.  ArrayList terletak pada kelas java.util jadi kalau ingin menggunakan ArrayList jangan lupa meng-importkan java.util.ArrayList. Ini membangun/membuat sebuah Scanner baru yang menghasilkan nilai-nilai yang dipindai dari file yang ditentukan. Data adalah nama scanner yang akan di buat , kita bisa merubah sesuai dengan data yang akan kita buat , yang terpenting adalah tidak membuat eror atau anda kebingungan. random untuk mengacak angka. string adalah jenis variable

--- src/com/reza/app12141378/Tebakangka.java
+++ src/com/reza/app12141378/Tebakangka.java
@@ -3,3 +3,12 @@ package com.reza.app12141378;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
+
+public class Tebakangka {
+    public static void main(String[] args) {
+        ArrayList <String> namauser = new ArrayList<>(); // Membuat array Nama user
+        ArrayList <Integer> skor = new ArrayList<>(); // Membuat array Sekor user
+        Scanner input1 = new Scanner(System.in); // Input pertama 
+        Scanner input2= new Scanner(System.in); // Input kedua
+        Random random1=new Random(); // Mengacak angka
+        String cariY; // Untuk mencari nilai Y, jika input y, maka otomatis akan keluar
